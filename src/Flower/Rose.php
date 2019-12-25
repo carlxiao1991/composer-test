a51b359ae1641ba2374a2442ad30c2769ab64974
Carl <Carl@dextask.com>
2019-12-25 11:16:58 +0800

    composer-test

---
+++ src/Flower/Rose.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Flower\Rose;
+
+/**
+ * Description of Rose
+ *
+ * @author Administrator
+ */
+class Rose {
+
+    public function desc() {
+        echo "this is rose flower";
+    }
+
+}
